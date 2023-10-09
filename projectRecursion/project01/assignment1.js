e99ae8d0218a1075571f67132e6e9838a8774c1a
ztanvir <zahirulopel@gmail.com>
2023-10-09 16:43:01 +0600

    Add solution number 2 for asigment1

--- projectRecursion/project01/assignment1.js
+++ projectRecursion/project01/assignment1.js
@@ -7,3 +7,25 @@ const fibs = (fibsN) => {
   }
   return fibSeq;
 };
+
+// Fibonacci sequence with recursion
+const fibsRec = (fibsN) => {
+  let fibArr = [];
+
+  for (let i = 0; i < fibsN; i++) {
+    const fibNumber = (n) => {
+      // base case
+      if (n === 0) {
+        return 0;
+      } else if (n === 1) {
+        return 1;
+      }
+      //  recursive function
+      else {
+        return fibNumber(n - 1) + fibNumber(n - 2);
+      }
+    };
+    fibArr = fibArr.concat(fibNumber(i));
+  }
+  return fibArr;
+};
